9abc41c465836a472b379baec66d36518881d598
Eldaern <la150798@student.helha.be>
2017-05-08 12:15:02 +0200

    ce commit est désabusé

--- projetTournoi/Form1.cs
+++ projetTournoi/Form1.cs
@@ -522,9 +522,7 @@ namespace projetTournoi
             PreviousPanel = 6;
             CurentPanel = 8;
             BackButton.Visible = true;
-            DataSet ds =tournoiOBj.DetailTournoiDS((int)LT_DataGrid.CurrentRow.Cells["numéro"].Value);
-            
-
+            DataSet ds = tournoiOBj.DetailTournoiDS((int)LT_DataGrid.CurrentRow.Cells["numéro"].Value);
         }
     }
 }
